bcd518a34200e03d1975b4565f10fd251c72c541
fenglin913 <61560925+fenglin913@users.noreply.github.com>
2024-05-01 01:10:55 +1000

    add comments for wall_follower.py

--- wall_follower_pkg/wall_follower.py
+++ wall_follower_pkg/wall_follower.py
@@ -9,30 +9,30 @@ class WallFollower(Node):
     def __init__(self):
         super().__init__("wall_follower")
         self.set_parameters()
-        self.create_subscriptions()
+        self.create_subscribers()
         self.create_publishers()
         self.log_message("Wall Follower Node Initialized")
 
     def set_parameters(self):
-        self.max_wall_dist = 0.65  # Desired wall distance threshold
-        self.curr_state = 0
-        self.wall_found = False
-        self.start_received = False
-
-    def create_subscriptions(self):
+        self.threshold = 0.65  # wall distance threshold
+        self.curr_state = 0  #start with find a wall
+        self.wall_found = False  #inital with wall not find
+        self.start_received = False  # wait to start
+    #create subscribers
+    def create_subscribers(self):
         self.scan_subscription = self.create_subscription(
             LaserScan, "/scan", self.scan_callback, 10
         )
         self.start_subscription = self.create_subscription(
             Bool, "/start", self.start_callback, 10
         )
-
+    #create publisher
     def create_publishers(self):
         self.cmd_vel_publisher = self.create_publisher(Twist, "/cmd_vel", 10)
 
     def scan_callback(self, msg):
-        regions = self.process_laser_scan(msg)
-        self.take_action(regions)
+        sections = self.divide_sections(msg)
+        self.make_move(sections)
 
     def start_callback(self, msg):
         if msg.data:
@@ -41,61 +41,61 @@ class WallFollower(Node):
         else:
             self.log_message("Stopping wall following")
             self.start_received = False
-
-    def process_laser_scan(self, msg):
+    #divide laser read into 5 sections
+    def divide_sections(self, msg):
         length = len(msg.ranges)
         each_div = length // 8
         front_start = length - each_div // 2
         front_end = each_div // 2
 
-        regions = {
+        sections = {
             "front": min(min(msg.ranges[front_start:]), min(msg.ranges[:front_end])),
             "left": min(msg.ranges[each_div : 2 * each_div]),
             "fleft": min(msg.ranges[each_div // 2 : each_div]),
             "fright": min(msg.ranges[front_start - each_div // 2 : front_start]),
             "right": min(msg.ranges[length - 2 * each_div : length - each_div]),
         }
-        return regions
+        return sections
 
-    def take_action(self, regions):
+    def make_move(self, sections):
         if not self.wall_found:
             if (
-                regions["front"] > self.max_wall_dist
-                and regions["right"] > self.max_wall_dist
-                and regions["left"] > self.max_wall_dist
+                sections["front"] > self.threshold
+                and sections["right"] > self.threshold
+                and sections["left"] > self.threshold
             ):
                 self.curr_state = 0  # Find wall
-            elif regions["front"] < self.max_wall_dist:
-                self.curr_state = 1  # Wall found, turn left
-                self.log_message("wall found, turn left")
+            elif sections["front"] < self.threshold:
+                self.curr_state = 1  # found wall, turn left
+                self.log_message("found wall, turn left")
                 self.wall_found = True
         else:
             if (
-                regions["right"] > self.max_wall_dist
-                and regions["front"] > self.max_wall_dist
+                sections["right"] > self.threshold
+                and sections["front"] > self.threshold
             ):
-                self.curr_state = 3  # Move diagonally right
-                self.log_message("Move diagonally right")
+                self.curr_state = 3  # make right U turn
+                self.log_message("make right U turn")
             elif (
-                regions["right"] > self.max_wall_dist
-                and regions["fright"] > self.max_wall_dist
-                and regions["front"] > self.max_wall_dist
+                sections["right"] > self.threshold
+                and sections["fright"] > self.threshold
+                and sections["front"] > self.threshold
             ):
-                self.curr_state = 3  # Turn right
-                self.log_message("Turn right")
-            elif regions["front"] > self.max_wall_dist:
-                self.curr_state = 2  # Go straight
-                self.log_message("Go straight")
-            elif regions["front"] < self.max_wall_dist:
-                self.curr_state = 1  # Turn left
-                self.log_message("Turn left")
+                self.curr_state = 3  # turn right
+                self.log_message("turn right")
+            elif sections["front"] > self.threshold:
+                self.curr_state = 2  # move forward
+                self.log_message("move forward")
+            elif sections["front"] < self.threshold:
+                self.curr_state = 1  # turn left
+                self.log_message("turn left")
             elif (
-                regions["front"] < self.max_wall_dist
-                and regions["fleft"] < self.max_wall_dist
-                and regions["fright"] < self.max_wall_dist
+                sections["front"] < self.threshold
+                and sections["fleft"] < self.threshold
+                and sections["fright"] < self.threshold
             ):
                 self.curr_state = 1  # Dead end, turn left
-                self.log_message("Deade end, turn left")
+                self.log_message("deade end, turn left")
 
     def run(self):
         self.log_message("Wall Follower Started")
@@ -110,16 +110,16 @@ class WallFollower(Node):
 
     def get_velocity_command(self):
         vel_cmd = Twist()
-        if self.curr_state == 0:  # Find wall
+        if self.curr_state == 0:  # find a wall
             vel_cmd.linear.x = 0.15
             vel_cmd.angular.z = 0.0
-        elif self.curr_state == 1:  # Turn left
+        elif self.curr_state == 1:  # turn left
             vel_cmd.linear.x = 0.0
             vel_cmd.angular.z = 0.25
-        elif self.curr_state == 2:  # Go straight
+        elif self.curr_state == 2:  # move forward
             vel_cmd.linear.x = 0.07
             vel_cmd.angular.z = 0.0
-        elif self.curr_state == 3:  # Turn right or move diagonally right
+        elif self.curr_state == 3:  # right U turn
             vel_cmd.linear.x = 0.08
             vel_cmd.angular.z = -0.25
         else:
